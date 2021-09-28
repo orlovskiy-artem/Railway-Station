2ada4489f7e2c9d47c71230340c89acf383f3ae6
orlovskiy-artem <artem.orlovsky@gmail.com>
2021-09-28 05:41:30 +0300

    updated customerDAO

--- src/main/java/com/kpi/railwaystation/DAO/CustomerDAO.java
+++ src/main/java/com/kpi/railwaystation/DAO/CustomerDAO.java
@@ -3,11 +3,16 @@ package com.kpi.railwaystation.DAO;
 import com.kpi.railwaystation.model.Customer;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public interface CustomerDAO extends DAO{
-    public Customer createCustomer();
-    public Customer readCustomer();
-    public Customer updateCustomer();
-    public Customer deleteCustomer();
+    void createCustomer(String firstName, String lastName,
+                        String email, String password, Integer roleId) throws SQLException;
+
+    void readCustomerById(int customerId) throws SQLException;
+
+    void updateCustomer(HashMap<String, Object> columnsNames2values, String identifier) throws SQLException;
+
+    void deleteCustomerById(Integer id) throws SQLException;
 }
 
