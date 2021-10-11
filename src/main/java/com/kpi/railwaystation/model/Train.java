1300832bc316620492a5da93371e689544ada60c
orlovskiy-artem <artem.orlovsky@gmail.com>
2021-10-11 18:10:01 +0300

    add lombok to Train.java

--- src/main/java/com/kpi/railwaystation/model/Train.java
+++ src/main/java/com/kpi/railwaystation/model/Train.java
@@ -1,10 +1,14 @@
 package com.kpi.railwaystation.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.io.Serializable;
 
 @Data
+@Builder
+@NonNull
 public class Train implements Serializable {
     private Integer id;
     private String name;
